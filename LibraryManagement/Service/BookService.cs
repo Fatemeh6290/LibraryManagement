0abd4308b8e1c873823b5f1000a881a2c7a14a2c
Fatemeh Naseri <f.naseri@cp-pro.de>
2026-08-07 11:59:22 +0200

    BookServices Methods wird vollständig

--- LibraryManagement/Service/BookService.cs
+++ LibraryManagement/Service/BookService.cs
@@ -1,6 +1,49 @@
+using LibraryManagement.Model;
+
 namespace LibraryManagement.Service;
 
 public class BookService
 {
+    private readonly List<Book> _books = new();
+    public void AddBook(string title, string author)
+    {
+      _books.Add(new Book
+      {
+          BookId = _books.Count + 1,
+          Title = title,
+          Author = author
+      });
+    }
+
+    public List<Book> GetBooks()
+    {
+        return _books.ToList();
+    }
+
+    public Book? GetBookById(int id)
+    {
+        return _books.FirstOrDefault(x => x.BookId == id);
+    }
+
+    public bool DeleteBook(int id)
+    {
+        Book? book = GetBookById(id);
+        if (book is not null)
+        {
+            _books.Remove(book);
+            return true;
+        }
+        
+        return false;
+    }
+
+    public List<Book> SearchByTitle(string title)
+    {
+        return _books.Where(x => x.Title.Contains(title)).ToList();
+    }
     
+    public List<Book> SearchByAuthor(string author)
+    {
+        return _books.Where(x => x.Author == author).ToList();
+    }
 }
